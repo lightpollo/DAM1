44908ee3fb38eea3e16f018acf5f0385e865b648
ruben <ruben@DESKTOP-PUQVIUJ>
2018-02-13 12:08:17 +0100

    Prueba Git

---
+++ EjerciciosJava/src/ejerciciosjava/EjerciciosJava.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciosjava;
+
+/**
+ *
+ * @author ruben
+ */
+public class EjerciciosJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nombre = "Antonio Pérez Romero";
+        int edad = 20;
+        double altura = 1.75;
+        System.out.println("Me llamo"+ nombre+ " ,tengo"+ edad+ "años y mido"+ altura);
+    }
+    
+}
